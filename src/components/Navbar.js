8510f81e47c2fe1436017b5bb568d211f58cff11
abhinavxox <abhinavxox@gmail.com>
2022-09-03 19:16:41 +0545

    <added>:[toggle responsive bar added]

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../index.css'
+import { FaBars } from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 const Navbar = () => {
+
     return (
-        <div class="ui large secondary pointing menu fontCustom" style={{ background: 'white', marginBottom: '2px' }}>
-            <NavLink to='/' className="item" exact={true}>Home</NavLink>
-            <NavLink to='/about' className="item">About</NavLink>
-            <NavLink to='/work' className="item">Work</NavLink>
-            <NavLink to='/contact' className="item">Contact</NavLink>
+        <div className='ui large secondary pointing menu' style={{ background: 'white', marginBottom: '2px' }}>
+            <a href='#' className='toggle-button'>
+                <IconContext.Provider value={{ className: "shared-class", size: 70 }}>
+                    <FaBars size={40} />
+                </IconContext.Provider>
+            </a>
+            <div className="ui large secondary pointing menu fontCustom before-navbar">
+                <NavLink to='/' className="item" exact={true}>Home</NavLink>
+                <NavLink to='/about' className="item">About</NavLink>
+                <NavLink to='/work' className="item">Work</NavLink>
+                <NavLink to='/contact' className="item">Contact</NavLink>
+            </div>
         </div>
     )
 }
