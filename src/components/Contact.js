6b9e2cbb3cd804db6f55f00d03a7428e616df134
abhinavxox <abhinavxox@gmail.com>
2022-09-03 18:23:54 +0545

    <completed ui>:[just static part completed]

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,64 @@
 import React from 'react';
 import '../index.css'
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Contact = () => {
     return (
         <div className='contact'>
-            <div className='ui very padded segment'>
+            <div className='ui pad segment'>
                 <div className='ui two column stackable grid '>
-                    <div className="column"> Hello</div>
-                    <div className="headerDivider"></div>
-                    <div className="column"> Hello</div>
+                    <div className="ui text container fontCustom column">
+                        <div class="ui two column grid">
+                            <div class="column contact-left">
+                                <div class="ui raised segment">
+                                    <a class="ui red ribbon label">Contact Information</a>
+                                    <p>
+                                    </p>
+                                    <div className='ui very padded segment'>
+                                        <div className='row pad'>
+                                            <BsFillTelephoneFill color='green' /> <p>+91 9153563744, +977 9841085450</p>
+                                        </div>
+                                        <div className='row pad'>
+                                            <FaGithub /> <p><a href='https://github.com/Abhinavxox' target='_blank' style={{ color: 'black' }}> Abhinavxox</a></p>
+                                        </div>
+                                        <div className='row pad'>
+                                            <FaLinkedin color='#0A66C2' /> <p><a href='https://www.linkedin.com/in/abhinav-pandey-480269176/' target='_blank' style={{ color: 'black' }}> Abhinav Pandey</a></p>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="ui text container column fontCustom">
+                        <form class="ui form attached fluid segment">
+                            <div class="two fields">
+                                <div class="field">
+                                    <label>First Name</label>
+                                    <input placeholder="First Name" type="text" />
+                                </div>
+                                <div class="field">
+                                    <label>Last Name</label>
+                                    <input placeholder="Last Name" type="text" />
+                                </div>
+                            </div>
+                            <div class="field">
+                                <label>Mail</label>
+                                <input placeholder="Mail" type="text" />
+                            </div>
+                            <div class="field">
+                                <label>Message</label>
+                                <textarea placeholder="Write something.." style={{ height: '200px' }}></textarea>
+                            </div>
+                            <div class="inline field">
+                            </div>
+                            <div class="ui blue submit button fontCustom">Send</div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
